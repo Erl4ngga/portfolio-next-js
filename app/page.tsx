3ff1a89a415201792bdf9008e1cd133ec6553fba
Erl4ngga <widhoerlangga212@gmail.com>
2023-08-24 03:41:29 +0700

    deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,71 @@
 import NextLink from "next/link";
 import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
+import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
+import CardTwitter from "@/components/card";
+import ProjectCard from "@/components/project";
+import Certificate from "@/components/certificate";
 
 export default function Home() {
-	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-				<br />
-				<h1 className={title()}>
-					websites regardless of your design experience.
-				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
-				</h2>
-			</div>
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-lg text-center justify-center">
+        <h1 className={title()}>Hi,I am&nbsp;</h1>
+        <h1 className={title({ color: "violet" })}>FullStack&nbsp;</h1>
+        <br />
+        <h1 className={title()}>Developer</h1>
+      </div>
 
-			<div className="flex gap-3">
-				<Link
-					isExternal
-					as={NextLink}
-					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-				>
-					Documentation
-				</Link>
-				<Link
-					isExternal
-					as={NextLink}
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href={siteConfig.links.github}
-				>
-					<GithubIcon size={20} />
-					GitHub
-				</Link>
-			</div>
-
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
-			</div>
-		</section>
-	);
+      <div className="flex gap-3 py-5">
+        <Link
+          isExternal
+          as={NextLink}
+          href={siteConfig.links.docs}
+          className={buttonStyles({
+            color: "primary",
+            radius: "full",
+            variant: "shadow",
+          })}
+        >
+          Documentation
+        </Link>
+        <Link
+          isExternal
+          as={NextLink}
+          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          href={siteConfig.links.github}
+        >
+          <GithubIcon size={20} />
+          GitHub
+        </Link>
+      </div>
+      <div className="py-6 md:px-3">
+        <CardTwitter />
+      </div>
+      <div>
+        <h2
+          className={subtitle({ class: "mt-6 font-bold text-white text-2xl" })}
+        >
+          My Project
+        </h2>
+      </div>
+      <div>
+        <ProjectCard />
+      </div>
+      <div>
+        <h2
+          className={subtitle({ class: "mt-6 font-bold text-white text-2xl" })}
+        >
+          certificate
+        </h2>
+      </div>
+      <div>
+        <Certificate />
+      </div>
+    </section>
+  );
 }
---
+++ components/certificate.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import { Image } from "@nextui-org/react";
+
+export default function Certificate() {
+  return (
+    <Image
+      width={1000}
+      height={200}
+      alt="NextUI hero Image with delay"
+      src="/Certificate.png"
+    />
+  );
+}
