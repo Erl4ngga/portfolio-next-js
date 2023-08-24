4cc17696ed1af19501976db237674bc78261dd76
Erl4ngga <widhoerlangga212@gmail.com>
2023-08-24 16:58:56 +0700

    add project photo

--- components/project.tsx
+++ components/project.tsx
@@ -14,12 +14,8 @@ export default function ProjectCard() {
     <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
       <Card className="col-span-12 sm:col-span-4 h-[300px]">
         <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-          <p className="text-tiny text-black uppercase font-bold">
-            What to watch
-          </p>
-          <h4 className="text-black font-medium text-large">
-            Stream the Acme event
-          </h4>
+          <p className="text-tiny text-black uppercase font-bold">php 7.4</p>
+          <h4 className="text-black font-medium text-large">laravel-cashier</h4>
         </CardHeader>
         <Image
           removeWrapper
@@ -30,10 +26,8 @@ export default function ProjectCard() {
       </Card>
       <Card className="col-span-12 sm:col-span-4 h-[300px]">
         <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-          <p className="text-tiny text-white/60 uppercase font-bold">
-            Plant a tree
-          </p>
-          <h4 className="text-white font-medium text-large">
+          <p className="text-tiny text-black uppercase font-bold">Php 8.1</p>
+          <h4 className="text-black font-medium text-large">
             Contribute to the planet
           </h4>
         </CardHeader>
@@ -46,11 +40,9 @@ export default function ProjectCard() {
       </Card>
       <Card className="col-span-12 sm:col-span-4 h-[300px]">
         <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-          <p className="text-tiny text-white/60 uppercase font-bold">
-            Supercharged
-          </p>
-          <h4 className="text-white font-medium text-large">
-            Creates beauty like a beast
+          <p className="text-tiny text-black uppercase font-bold">php 8.1</p>
+          <h4 className="text-black font-medium text-large">
+            fresh shop laravel 10
           </h4>
         </CardHeader>
         <Image
@@ -65,8 +57,8 @@ export default function ProjectCard() {
         className="w-full h-[300px] col-span-12 sm:col-span-5"
       >
         <CardHeader className="absolute z-10 top-1 flex-col items-start">
-          <p className="text-tiny text-white/60 uppercase font-bold">New</p>
-          <h4 className="text-black font-medium text-2xl">Acme camera</h4>
+          <p className="text-tiny text-black uppercase font-bold">php 7.4</p>
+          <h4 className="text-black font-medium text-2xl">masjid laravel 7</h4>
         </CardHeader>
         <Image
           removeWrapper
@@ -89,11 +81,9 @@ export default function ProjectCard() {
         className="w-full h-[300px] col-span-12 sm:col-span-7"
       >
         <CardHeader className="absolute z-10 top-1 flex-col items-start">
-          <p className="text-tiny text-black uppercase font-bold">
-            Your day your way
-          </p>
+          <p className="text-tiny text-black uppercase font-bold">php 8.1</p>
           <h4 className="text-black font-medium text-xl">
-            Your checklist for better sleep
+            management product laravel 10
           </h4>
         </CardHeader>
         <Image
