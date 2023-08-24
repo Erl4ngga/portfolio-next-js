911dbd837fe71c87817289bc87e06c84e1eb2cbe
Erl4ngga <widoerlangga9@gmail.com>
2023-08-24 20:02:41 +0700

    add meta data

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
+  name: "Next UI Portfolio",
   description: "Make beautiful websites regardless of your design experience.",
   navItems: [
     {
