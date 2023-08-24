2de6235f3fde0cc55ad7a061fd1c448a86c6d142
Erl4ngga <widoerlangga9@gmail.com>
2023-08-24 18:59:14 +0700

    fixde

--- components/card.tsx
+++ components/card.tsx
@@ -13,7 +13,7 @@ export default function CardTwitter() {
   const [isFollowed, setIsFollowed] = React.useState(false);
 
   return (
-    <Card className="max-w-[340px] text-left">
+    <Card className="max-w-[340px] text-left border shadow-xl">
       <CardHeader className="justify-between">
         <div className="flex gap-5">
           <Avatar
@@ -22,19 +22,19 @@ export default function CardTwitter() {
             size="md"
             src="/avatars/avatar-1.png"
           />
-          <div className="flex flex-col gap-1 items-start justify-center">
+          <div className="flex flex-col gap-1 items-start justify-center mr-2">
             <h4 className="text-small font-semibold leading-none text-default-600">
               erlangga
             </h4>
-            <h5 className="text-small tracking-tight text-default-400">
-              @zoeylang
+            <h5 className="text-small tracking-tight text-default-400 ">
+              @erlang_stack
             </h5>
           </div>
         </div>
         <Button
           className={
             isFollowed
-              ? "bg-transparent text-foreground border-default-200"
+              ? "bg-transparent text-foreground border-default-200 "
               : ""
           }
           color="primary"
@@ -47,24 +47,18 @@ export default function CardTwitter() {
         </Button>
       </CardHeader>
       <CardBody className="px-3 py-0 text-small text-default-400">
-        <p>
-          Frontend developer and UI/UX enthusiast. Join me on this coding
-          adventure!
-        </p>
+        <p>Software Engineer / エルランガ</p>
         <span className="pt-2">
-          #FrontendWithZoey
-          <span className="py-2" aria-label="computer" role="img">
-            💻
-          </span>
+          <span className="py-2" aria-label="computer" role="img"></span>
         </span>
       </CardBody>
       <CardFooter className="gap-3">
         <div className="flex gap-1">
-          <p className="font-semibold text-default-400 text-small">4</p>
+          <p className="font-semibold text-default-400 text-small">381</p>
           <p className=" text-default-400 text-small">Following</p>
         </div>
         <div className="flex gap-1">
-          <p className="font-semibold text-default-400 text-small">97.1K</p>
+          <p className="font-semibold text-default-400 text-small">14</p>
           <p className="text-default-400 text-small">Followers</p>
         </div>
       </CardFooter>
--- components/project.tsx
+++ components/project.tsx
@@ -8,6 +8,7 @@ import {
   Image,
   Button,
 } from "@nextui-org/react";
+import Link from "next/link";
 
 export default function ProjectCard() {
   return (
@@ -28,14 +29,14 @@ export default function ProjectCard() {
         <CardHeader className="absolute z-10 top-1 flex-col !items-start">
           <p className="text-tiny text-black uppercase font-bold">Php 8.1</p>
           <h4 className="text-black font-medium text-large">
-            Contribute to the planet
+            management product laravel-10
           </h4>
         </CardHeader>
         <Image
           removeWrapper
           alt="Card background"
           className="z-0 w-full h-full object-cover"
-          src="/images/card-example-3.jpeg"
+          src="/gro-mart.png"
         />
       </Card>
       <Card className="col-span-12 sm:col-span-4 h-[300px]">
@@ -90,7 +91,7 @@ export default function ProjectCard() {
           removeWrapper
           alt="Relaxing app background"
           className="z-0 w-full h-full object-cover"
-          src="/gro-mart.png"
+          src="/flowered.png"
         />
         <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
           <div className="flex flex-grow gap-2 items-center">
@@ -106,8 +107,8 @@ export default function ProjectCard() {
               </p>
             </div>
           </div>
-          <Button radius="full" size="sm">
-            Get App
+          <Button color="primary" variant="shadow" size="sm">
+            <Link href="https://gro-mart.top/">GitHub</Link>
           </Button>
         </CardFooter>
       </Card>
